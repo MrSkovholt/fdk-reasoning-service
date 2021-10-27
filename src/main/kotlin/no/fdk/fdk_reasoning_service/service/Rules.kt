da86e6b4c730ca689c9867fd8a1d95f6c665cdfb
Nils Ove Tendenes <50194012+NilsOveTen@users.noreply.github.com>
2021-10-27 12:01:29 +0200

    Feat/not/add rule for nap relation (#7)
    * feat: rules for NAP related datasets
    
    * chore: single statement for nap theme
    
    * refactor: rm joinToString body
    
    Co-authored-by: Jeff Reiffers <jeffreiffers@gmail.com>

--- src/main/kotlin/no/fdk/fdk_reasoning_service/service/Rules.kt
+++ src/main/kotlin/no/fdk/fdk_reasoning_service/service/Rules.kt
@@ -1,15 +1,24 @@
 package no.fdk.fdk_reasoning_service.service
 
-const val datasetRules = """
+val datasetRules = """
     @prefix dcat: <http://www.w3.org/ns/dcat#> .
     @prefix dct: <http://purl.org/dc/terms/> .
     @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
     @prefix fdk: <https://raw.githubusercontent.com/Informasjonsforvaltning/fdk-reasoning-service/master/src/main/resources/ontology/fdk.owl#> .
 
     [isAuthoritative:
-        (?dataset rdf:type dcat:Dataset)
-        (?dataset dct:provenance ?provenance)
+        (?dataset rdf:type dcat:Dataset),
+        (?dataset dct:provenance ?provenance),
         equal(?provenance, <http://data.brreg.no/datakatalog/provinens/nasjonal>)
-
         -> (?dataset fdk:isAuthoritative 'true'^^xsd:boolean) ]
+
+    [isRelatedToTransportportal:
+        (?dataset rdf:type dcat:Dataset),
+        (?dataset dct:accessRights ?rights),
+        equal(?rights, <http://publications.europa.eu/resource/authority/access-right/PUBLIC>),
+        (?dataset dcat:theme ?theme),
+        strConcat(?theme,?themeStr),
+        regex(?themeStr, '${napThemes.joinToString(separator = "|")}')
+        -> (?dataset fdk:isRelatedToTransportportal 'true'^^xsd:boolean)
+    ]
 """
