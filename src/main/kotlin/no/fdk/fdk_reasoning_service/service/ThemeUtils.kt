ed361cfe04b866d05c67f9fc46e2c3aed85b85fb
NilsOveTen <github.nils.ove@tendenes.com>
2024-01-04 13:11:39 +0100

    refactor: rm deprecated statement selector

--- src/main/kotlin/no/fdk/fdk_reasoning_service/service/ThemeUtils.kt
+++ src/main/kotlin/no/fdk/fdk_reasoning_service/service/ThemeUtils.kt
@@ -4,17 +4,9 @@ import no.fdk.fdk_reasoning_service.model.ExternalRDFData
 import no.fdk.fdk_reasoning_service.rdf.FDK
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdf.model.ModelFactory
-import org.apache.jena.rdf.model.SimpleSelector
-import org.apache.jena.rdf.model.Statement
-
-private class ThemeTripleSelector: SimpleSelector() {
-    override fun test(s: Statement): Boolean {
-        return s.predicate == FDK.themePath
-    }
-}
 
 fun ExternalRDFData.selectedThemeTriples(): Model =
     ModelFactory.createDefaultModel()
-        .add(losData.listStatements(ThemeTripleSelector()))
-        .add(eurovocs.listStatements(ThemeTripleSelector()))
-        .add(dataThemes.listStatements(ThemeTripleSelector()))
+        .add(losData.listStatements().filterKeep { s -> s.predicate == FDK.themePath }.toList())
+        .add(eurovocs.listStatements().filterKeep { s -> s.predicate == FDK.themePath }.toList())
+        .add(dataThemes.listStatements().filterKeep { s -> s.predicate == FDK.themePath }.toList())
